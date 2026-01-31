7fa00612e4e9d4e03392f334eb19aa0732d171da
Remo Revan <palan25@outlook.com>
2026-01-31 04:51:02 +0000

    Updated providers.tf

--- providers.tf
+++ providers.tf
@@ -4,10 +4,15 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 
   backend "azurerm" {}
 }
+
 
 provider "azurerm" {
   features {}
